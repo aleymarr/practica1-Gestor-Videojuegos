3e3777921560caf22a98203555eda08cc8650228
aleymarr <aleyba2002@gmail.com>
2025-09-19 10:43:24 +0100

    Feature: Creacion de la clase VideoGame con sus atributos

---
+++ VideoGame.java
@@ -0,0 +1,11 @@
+public class VideoGame {
+    
+    String id;
+    String titulo;
+    String genero;
+    String compania;
+    int fechaLanzamiento;
+    int horasJugadas;
+    String id_favorito;
+
+}
